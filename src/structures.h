b703d88ff66ed3b63f16b00599fdaab1279f48a2
Amber <ambermalpas@gmail.com>
2025-11-04 15:41:11 -0500

    Add support for multiple sources in lightcurve generation and plotting
    - Implemented a strict flux conservation sanity check in the plotting module to ensure the sum of source fluxes aligns with the true flux.
    - Enhanced the plotting functions to optionally display fluxes for two sources.
    - Updated the lightcurve output to include relative fluxes for both sources in the output files.
    - Modified the lightcurve generator to compute and store individual source magnifications for binary events.
    - Adjusted the runner to correctly reference parameter files for binary source cases. (smoke bug)
    - Expanded the event structure to accommodate magnifications for both sources.
    - Ensured memory allocation for new source magnification vectors during setup.
    - Fixed malformed fits files from outputLightcurve.cpp (src bug)

--- src/structures.h
+++ src/structures.h
@@ -358,6 +358,8 @@ struct event{
   vector<double> Aobs;
   vector<double> Aerr;
   vector<double> Afit;
+    vector<double> Asrc1; //magnification of source 1
+  vector<double> Asrc2; //magnification of source 2
   vector<bool> nosat;      /*Is point unsaturated? */
   vector<double> backmag;
   vector<double> dF;
